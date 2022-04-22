2cfdc351140d67212942c7881f2d53f29ec703a7
malibinYun <nightmare73@naver.com>
2022-04-22 21:49:44 +0900

    remote mapper 작성

---
+++ app/src/main/java/com/malibin/study/trying/mvvm/data/remote/mapper/DiariesRemoteMapper.kt
@@ -0,0 +1,49 @@
+package com.malibin.study.trying.mvvm.data.remote.mapper
+
+import com.malibin.study.trying.mvvm.data.remote.params.SaveDiaryParams
+import com.malibin.study.trying.mvvm.data.remote.params.UpdateDiaryParams
+import com.malibin.study.trying.mvvm.data.remote.response.DiaryResponse
+import com.malibin.study.trying.mvvm.domain.Diary
+import java.util.*
+
+class DiariesRemoteMapper {
+    fun toDiary(diaryResponse: DiaryResponse): Diary = Diary(
+        id = diaryResponse.id,
+        title = diaryResponse.title,
+        content = diaryResponse.content,
+        createDate = Date(diaryResponse.createdAt),
+    )
+
+    fun toSaveDiaryParams(diary: Diary): SaveDiaryParams = SaveDiaryParams(
+        title = diary.title,
+        content = diary.content,
+        createdAt = diary.createDate.time,
+        id = diary.id,
+    )
+
+    fun toUpdateDiaryParams(diary: Diary): UpdateDiaryParams = UpdateDiaryParams(
+        title = diary.title,
+        content = diary.content,
+        id = diary.id,
+    )
+}
+
+fun DiaryResponse.toDiary(): Diary = Diary(
+    id = this.id,
+    title = this.title,
+    content = this.content,
+    createDate = Date(this.createdAt),
+)
+
+fun Diary.toSaveDiaryParams(): SaveDiaryParams = SaveDiaryParams(
+    title = this.title,
+    content = this.content,
+    createdAt = this.createDate.time,
+    id = this.id,
+)
+
+fun Diary.toUpdateDiaryParams(): UpdateDiaryParams = UpdateDiaryParams(
+    title = this.title,
+    content = this.content,
+    id = this.id,
+)
--- app/src/main/java/com/malibin/study/trying/mvvm/data/remote/source/DiariesRemoteSource.kt
+++ app/src/main/java/com/malibin/study/trying/mvvm/data/remote/source/DiariesRemoteSource.kt
@@ -1,23 +1,21 @@
 package com.malibin.study.trying.mvvm.data.remote.source
 
-import com.malibin.study.trying.mvvm.data.remote.params.SaveDiaryParams
-import com.malibin.study.trying.mvvm.data.remote.params.UpdateDiaryParams
+import com.malibin.study.trying.mvvm.data.remote.mapper.DiariesRemoteMapper
 import com.malibin.study.trying.mvvm.data.remote.service.MalibinService
 import com.malibin.study.trying.mvvm.data.source.DiariesSource
 import com.malibin.study.trying.mvvm.domain.Diary
 import retrofit2.Response
-import java.util.*
 
 class DiariesRemoteSource(
     private val malibinService: MalibinService,
+    private val diariesRemoteMapper: DiariesRemoteMapper,
 ) : DiariesSource {
     override suspend fun getAllDiaries(): Result<List<Diary>> {
         val response = malibinService.getDiaries()
         if (response.isSuccessful) {
-            val remoteDiaries = response.body().orEmpty()
-            val diaries =
-                remoteDiaries.map { Diary(it.id, it.title, it.content, Date(it.createdAt)) }
-            return Result.success(diaries)
+            return response.body().orEmpty()
+                .map { diariesRemoteMapper.toDiary(it) }
+                .let { Result.success(it) }
         }
         return Result.failure(IllegalStateException(response.message()))
     }
@@ -27,36 +25,18 @@ class DiariesRemoteSource(
         if (response.isSuccessful) {
             val remoteDiary = response.body()
                 ?: return Result.failure(IllegalStateException("response success but body is null!"))
-            val diary = Diary(
-                remoteDiary.id,
-                remoteDiary.title,
-                remoteDiary.content,
-                Date(remoteDiary.createdAt),
-            )
-            return Result.success(diary)
+            return Result.success(diariesRemoteMapper.toDiary(remoteDiary))
         }
         return Result.failure(IllegalStateException(response.message()))
     }
 
     override suspend fun saveDiary(diary: Diary): Result<Unit> {
-        return malibinService.saveDiary(
-            params = SaveDiaryParams(
-                title = diary.title,
-                content = diary.content,
-                createdAt = diary.createDate.time,
-                id = diary.id,
-            )
-        ).toResult()
+        return malibinService.saveDiary(params = diariesRemoteMapper.toSaveDiaryParams(diary))
+            .toResult()
     }
 
     override suspend fun updateDiary(diary: Diary): Result<Unit> {
-        return malibinService.updateDiary(
-            params = UpdateDiaryParams(
-                title = diary.title,
-                content = diary.content,
-                id = diary.id,
-            )
-        ).toResult()
+        return malibinService.updateDiary(diariesRemoteMapper.toUpdateDiaryParams(diary)).toResult()
     }
 
     override suspend fun deleteDiary(diaryId: String): Result<Unit> {
